1863ccd6a7f43acf4b6f66bc1fb4862aa1b480f4
khanhha <khanhhadsdl@gmail.com>
2022-05-22 20:42:37 +0700

    lap02_2842 (1)

---
+++ lap02_2842/IBook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lap02_2842
+{
+    interface IBook
+    {
+        string id { get; set;}
+        string tacgia { get; set; }
+        string tensach { get; set; }
+        string nxb { get; set; }
+
+        void show();
+    }
+}
