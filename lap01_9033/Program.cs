c8079a3634c8594821cc0c7aec74179918f913f3
khanhha <khanhhadsdl@gmail.com>
2022-05-22 20:22:38 +0700

    lap01_9033

---
+++ lap01_9033/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lap01_9033
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap so luong sinh vien: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Student[] sinhviens = new Student[n];
+            for(int i=0; i<n; i++)
+            {
+                Console.Write("Nhap SID: ");
+                string sid = Console.ReadLine();
+                Console.Write("Nhap ho ten: ");
+                string ht = Console.ReadLine();
+                Console.Write("Nhap khoa: ");
+                string kh = Console.ReadLine();
+                Console.Write("Nhap dtb: ");
+                string dtb = Console.ReadLine();
+ 
+                sinhviens[i] = new Student(sid, ht, kh, dtb);
+            }
+
+            Console.WriteLine("\n============DANH SACH SINH VIEN==========");
+            for (int i = 0; i<n; i++)
+            {
+                sinhviens[i].show();
+                Console.WriteLine("==========");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lap01_9033/Student.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lap01_9033
+{
+    class Student
+    {
+        string SID;
+        string hoten;
+        string khoa;
+        string dtb;
+
+        public Student() { }
+
+        public Student(string sID1, string hoten, string khoa, string dtb)
+        {
+            SID1 = sID1;
+            Hoten = hoten;
+            Khoa = khoa;
+            Dtb = dtb;
+        }
+
+        public string SID1 { get => SID; set => SID = value; }
+        public string Hoten { get => hoten; set => hoten = value; }
+        public string Khoa { get => khoa; set => khoa = value; }
+        public string Dtb { get => dtb; set => dtb = value; }
+
+        public void show()
+        {
+            Console.WriteLine("SID: " + SID);
+            Console.WriteLine("Ho ten: " + hoten);
+            Console.WriteLine("Khoa: " + khoa);
+            Console.WriteLine("Diem trung binh: " + dtb);
+        }
+    }
+}
